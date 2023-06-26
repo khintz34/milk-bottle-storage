335b65b7de1ffb03645124e8c97d8348f26e95e2
Kevin Hintz <khintz34@gmail.com>
2023-06-26 16:39:40 -0500

    resources pt2

--- src/app/Resources/page.js
+++ src/app/Resources/page.js
@@ -8,16 +8,19 @@ export default function Resources() {
       <Resource
         name="Exlclusive Pumping"
         url="https://exclusivepumping.com/breast-milk-freezer-stash-calculator/"
+        desc="More information on storing breast milk and finding pump end date."
       />
 
       <Resource
         name="Woman Calculators"
         url="https://womencalculators.com/when-can-i-stop-pumping-calculator/"
+        desc="When Can I Stop Pumping Calculator"
       />
 
       <Resource
         name="Bumble Baby"
         url="https://bumblebabychicago.com/how-to-efficiently-store-breastmilk-how-to-make-breastmilk-bricks/#:~:text=What%20the%20heck%20is%20a,when%20you%20go%20to%20thaw!"
+        desc="How to store breast milk"
       />
     </main>
   );
--- src/components/ResourceComp.js
+++ src/components/ResourceComp.js
@@ -3,9 +3,15 @@ import react, { useState } from "react";
 import styles from "../styles/Resources.module.css";
 
 export default function Resource(props) {
+  function openTab() {
+    window.open(props.url);
+  }
   return (
     <main className={styles.main}>
-      {props.name}: {props.url}
+      <div onClick={openTab}>
+        <h3 className={styles.h3}>{props.name}</h3>
+        <p>{props.desc}</p>
+      </div>
     </main>
   );
 }
